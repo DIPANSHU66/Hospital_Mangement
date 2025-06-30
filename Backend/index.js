7c02e54229b77b1cfae4d3e09b8421df4dac03ad
DIPANSHU66 <dipanshu682004@gmail.com>
2025-06-30 18:18:19 +0530

    ready for deploy

--- Backend/index.js
+++ Backend/index.js
@@ -5,11 +5,11 @@ const { dbconnection } = require("../Backend/database/dbConnection");
 const messagerouter = require("../Backend/router/MessageRouter");
 const userRouter = require("../Backend/router/UserRouter");
 const appointmentrouter = require("../Backend/router/AppointmentRouter");
+const path = require("path");
 
 require("dotenv").config();
 
 const app = express();
-
 
 PORT = process.env.PORT || 4000;
 
@@ -27,13 +27,18 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-
-
 app.use("/api/v1/message", messagerouter);
 app.use("/api/v1/appointment", appointmentrouter);
 app.use("/api/v1/user", userRouter);
 
 dbconnection();
+
+const _dirname = path.resolve();
+
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(_dirname, "Frontend","dist","index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server Listen at Port :${PORT}`);
